9f6e490ae0fcc589c25f4dc1b74f371bcb380f5c
ingvildrokke <ingvildrokke@gmail.com>
2013-09-18 01:54:06 +0200

    Siste versjon javascript lab2

--- proglangs.js
+++ proglangs.js
@@ -3,13 +3,13 @@ function printToOutputBox(selectobj)
  var e_proglangs = document.getElementById("proglangs");
  var e_outputbox = document.getElementById("outputbox");
  e_outputbox.innerHTML = "Du har valgt: "+ e_proglangs.options[selectobj.selectedIndex].text;
-<<<<<<< HEAD
+
 }
 function myFunction()
 {
 alert("Siden er under konstruksjon!\nKom gjerne tilbake senere :)");
 }
-=======
+
 
 }
->>>>>>> 9be7c8827b3929f3307fe07b096ec310e754712c
+
