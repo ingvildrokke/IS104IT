febb0bffbe760e34c1ab4ab0cdfa60e53e1c95cc
ingvildrokke <ingvildrokke@gmail.com>
2013-09-18 01:51:52 +0200

    Siste versjon javascript lab2

--- proglangs.js
+++ proglangs.js
@@ -1,7 +1,10 @@
-var element_unordered_list = document.getElementById("first7");alert(element_unordered_list);element_unordered_list.style.color = "green";
-
-function printToOutputBox(selectobj){
+function printToOutputBox(selectobj)
+{
  var e_proglangs = document.getElementById("proglangs");
  var e_outputbox = document.getElementById("outputbox");
  e_outputbox.innerHTML = "Du har valgt: "+ e_proglangs.options[selectobj.selectedIndex].text;
+}
+function myFunction()
+{
+alert("Siden er under konstruksjon!\nKom gjerne tilbake senere :)");
 }
